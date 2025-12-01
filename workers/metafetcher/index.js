3ea068ebbf61ff8eb492de2b6ca1411c3894bb76
SLT World <sltworld.py@gmail.com>
2025-12-01 18:07:18 +0800

    Update index.js

--- workers/metafetcher/index.js
+++ workers/metafetcher/index.js
@@ -26,7 +26,7 @@ export default {
             method: "GET",
             redirect: "follow",
             signal,
-            cf: { scrapeShield: false }
+            cf: { scrapeShield: false },
             headers: {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0"
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
