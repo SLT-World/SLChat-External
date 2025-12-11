40d19a3d595c259e77513a04996fff484b9ab9e9
SLT World <sltworld.py@gmail.com>
2025-12-11 14:26:37 +0800

    Broken
    Google API version immediately malfunctioned on Chinese translation.

--- plugins/translator.js
+++ plugins/translator.js
@@ -5,15 +5,11 @@ async function translateMessage() {
     const text = messageElement.dataset.raw;
     if (!text) return;
     try {
-        const response = await fetch(`https://translate-pa.googleapis.com/v1/translateHtml`, {
-            method: "POST",
-            body: JSON.stringify([[[text],"auto","en"],"wt_lib"]),
-            headers: { "Content-type": "application/json+protobuf", "X-Goog-API-Key": "AIzaSyATBXajvzQLTDHEQbcpq0Ihe0vWDHmO520" }
-        });
+        const response = await fetch(`https://clients5.google.com/translate_a/t?client=dict-chrome-ex&sl=auto&tl=en&q=${encodeURIComponent(text)}`);
         if (!response.ok) throw new Error(`HTTP ${response.status}`);
         const data = await response.json();
         const translatedText = data[0][0];
-        const detectedLanguage = new Intl.DisplayNames(['en'], { type: 'language' }).of(data[1][0]);
+        const detectedLanguage = new Intl.DisplayNames(['en'], { type: 'language' }).of(data[0][1]);
         if (messageElement.classList.contains("message-container")) messageElement = messageElement.querySelector(".text.message");
         messageElement.innerHTML = parseMarkdown(translatedText) + `<p class="subtext" style="display: flex;align-items: center;gap: 5px;"><i class="bx bx-translate"></i>Translated from ${detectedLanguage} - <button class="button" onclick="reverseTranslate(${selectedMessage})" style="padding: 0;height: unset;background: transparent;color: cornflowerblue;">Original</button></p>`;
     }
